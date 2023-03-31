3738fc1d27f7fe477d805755f45e7b726c97273d
AslinurTolga <aslinurtolga@gmail.com>
2023-03-31 15:23:31 +0300

    feat: created endpoint from azure

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,9 +1,12 @@
 export async function GET(request: Request) {
   // Coonect to our Microsoft Azure Function endpoint
-  const response = await fetch("...", {
-    cache: "no-store",
-    // istek yanıtı önbelleklenmez
-  });
+  const response = await fetch(
+    "http://localhost:7071/api/getChatGPTSuggestion",
+    {
+      cache: "no-store",
+      // istek yanıtı önbelleklenmez
+    }
+  );
   const textData = await response.text();
 
   return new Response(JSON.stringify(textData.trim()), {
