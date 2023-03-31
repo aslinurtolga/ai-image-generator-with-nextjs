24d6b2f9ef9ec80154f78dfe33433a37301d04f0
AslinurTolga <aslinurtolga@gmail.com>
2023-03-31 14:45:24 +0300

    feat: request function created

---
+++ app/api/suggestion/route.ts
@@ -0,0 +1,13 @@
+export async function GET(request: Request) {
+  // Coonect to our Microsoft Azure Function endpoint
+  const response = await fetch("...", {
+    cache: "no-store",
+    // istek yanıtı önbelleklenmez
+  });
+  const textData = await response.text();
+
+  return new Response(JSON.stringify(textData.trim()), {
+    // trim: baştaki ve sondaki boşluk karakterleri siler
+    status: 200,
+  });
+}
